6c05c05822f2f92a8c0dd57b4b81ed8e0401df27
ksowah <sowahkelvin640@gmail.com>
2022-06-09 18:53:01 +0000

    create login screen

--- screens/Login.js
+++ screens/Login.js
@@ -1,19 +1,35 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Button, Image, Input } from '@rneui/themed';
+import { View, KeyboardAvoidingView } from 'react-native'
+import React, { useState } from 'react'
+import { Button, Image, Input } from '@rneui/base';
 import { StatusBar } from 'expo-status-bar';
 import tw from "twrnc"
 
-const Login = () => {
+const Login = ({ navigation }) => {
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const signIn = ({ navigation }) => {
+
+    }
+
   return (
-    <View>
+    <KeyboardAvoidingView behavior='padding' style={tw`flex-1 items-center justify-center p-10 bg-white`}>
       <StatusBar style='light'/>
-      <Image source={{
-        url: "https://png.pngtree.com/png-vector/20210117/ourmid/pngtree-signal-icon-logo-design-png-png-image_2761264.jpg"
-      }} 
+      <Image source={ require("../assets/signal-logo.png") } 
         style={tw`h-[15rem] w-[15rem] `}
       />
-    </View>
+
+      <View style={tw`w-[19rem]`}>
+        <Input placeholder='Email' autoFocus textContentType='emailAddress' value={email} onChangeText={text => setEmail(text)}/>
+        <Input placeholder='Password' secureTextEntry textContentType='password' value={password} onChangeText={text => setPassword(text)}/>
+      </View>
+
+      <Button style={tw`w-[13rem]`} color={"#2c6bed"} title={"Login"} onPress={signIn}/>
+      <Button style={tw`w-[13rem] mt-4`} title={"Register"} type="outline" onPress={() => navigation.navigate("Register")} />
+
+      <View style={tw`h-[5rem]`}/>
+    </KeyboardAvoidingView>
   )
 }
 
