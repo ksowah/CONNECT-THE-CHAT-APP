0eb23455458eb87ffc0bb2f7dcaabd34bffe9149
ksowah <sowahkelvin640@gmail.com>
2022-06-12 00:57:47 +0000

    create home

---
+++ screens/Home.js
@@ -0,0 +1,95 @@
+import { Text, SafeAreaView, ScrollView, View, TouchableOpacity } from 'react-native'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
+import CustomListItem from '../components/CustomListItem'
+import tw from "twrnc"
+import { Avatar } from '@rneui/themed'
+import { auth, db } from '../firebase'
+import { signOut } from "firebase/auth";
+import { Ionicons } from '@expo/vector-icons';  
+import { SimpleLineIcons } from '@expo/vector-icons'; 
+import { collection, getDocs, onSnapshot, orderBy, query } from 'firebase/firestore'
+
+
+const Home = ({navigation}) => {
+
+  const [chats, setChats] = useState([])
+
+
+  
+    
+    useEffect(() => {
+      onSnapshot(
+        query(collection(db, "chats")),
+        (snapshot) => {
+          setChats(snapshot.docs)
+        })
+    }, [db])
+
+
+  const signUserOut = () => {
+    signOut(auth).then(() => {
+      // Sign-out successful.
+      navigation.replace("Login")
+    }).catch((error) => {
+      alert(error)
+      // An error happened.
+    });
+    
+  }
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: "Chats",
+      headerStyle: tw`bg-white`,
+      headerTitleStyle: tw`text-black`,
+      headerTintColor: "black", // icons
+      headerLeft: () => (
+        <View>
+          <TouchableOpacity activeOpacity={0.5} onPress={signUserOut}>
+            <Avatar 
+              rounded
+              source={{uri: auth?.currentUser?.photoURL}} 
+              />
+          </TouchableOpacity>
+        </View>
+      ),
+      headerRight: () => (
+        <View style={tw`flex flex-row items-center`} >
+            <TouchableOpacity activeOpacity={0.5} style={tw`mr-6`} >
+            <Ionicons name="camera-outline" size={24} color="black" />
+            </TouchableOpacity>
+
+            <TouchableOpacity activeOpacity={0.5} onPress={() => navigation.navigate("AddChat")} >
+            <SimpleLineIcons name="pencil" size={20} color="black" />
+            </TouchableOpacity>
+        </View>
+      )
+    })
+  }, [navigation])
+
+  const enterChat = (id, chatName) => {
+    navigation.navigate("Chat", {
+      id,
+      chatName
+    })
+  }
+
+  return (
+    <SafeAreaView>
+      <ScrollView style={tw`h-[100%]`} >
+        {
+          chats.map((chat) => (
+            <CustomListItem 
+              key={chat.id}
+              id={chat.id}
+              chat={chat.data()}
+              enterChat={enterChat}
+            />
+          ))
+        }
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+export default Home
