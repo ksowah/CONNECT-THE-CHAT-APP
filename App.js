d7d6bea0280cd57e3b79e97d41d0addc24b0d9b4
ksowah <sowahkelvin640@gmail.com>
2022-06-12 00:55:53 +0000

    create new routes

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "./screens/Login";
 import Register from "./screens/Register";
+import Home from "./screens/Home";
+import AddChat from "./screens/AddChat";
+import ChatScreen from "./screens/ChatScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -20,6 +23,9 @@ export default function App() {
 			<Stack.Navigator screenOptions={globalScreenOptions}>
 				<Stack.Screen name="Login" component={Login} />
 				<Stack.Screen name="Register" component={Register} />
+				<Stack.Screen name="Home" component={Home} />
+				<Stack.Screen name="AddChat" component={AddChat} />
+				<Stack.Screen name="Chat" component={ChatScreen} />
 			</Stack.Navigator>
 		
 			<StatusBar style="auto" />
