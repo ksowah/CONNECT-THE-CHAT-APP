1a7bc837e7537fccd77811663c8881d7a3a7d0eb
ksowah <sowahkelvin640@gmail.com>
2022-06-12 00:58:15 +0000

    add register screen

--- screens/Register.js
+++ screens/Register.js
@@ -3,6 +3,8 @@ import React, { useLayoutEffect, useState } from "react";
 import tw from "twrnc";
 import { Button, Input } from "@rneui/base";
 import { StatusBar } from "expo-status-bar";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth } from "../firebase";
 
 const Register = ({ navigation }) => {
 	const [name, setName] = useState("");
@@ -10,55 +12,82 @@ const Register = ({ navigation }) => {
 	const [password, setPassword] = useState("");
 	const [imageUrl, setImageUrl] = useState("");
 
-    useLayoutEffect(() => {
-        navigation.setOptions({
-            headerBackTitle: "Login"
-        })
-    }, [navigation])
+	useLayoutEffect(() => {
+		navigation.setOptions({
+			headerBackTitle: "Login",
+		});
+	}, [navigation]);
 
-    const register = () => {
-
-    }
+	const register = () => {
+		createUserWithEmailAndPassword(auth, email, password)
+			.then((userCredential) => {
+				// Signed in
+				const user = userCredential.user;
+				if (user) {
+					navigation.replace("Home");
+					// ...
+				}
+				// ...
+			})
+			.then(() => {
+				updateProfile(auth.currentUser, {
+					displayName: name,
+					photoURL: imageUrl || "https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG.png"
+				  })
+				
+			})
+			.catch((error) => {
+				const errorCode = error.code;
+				alert(errorCode)
+				// ..
+			});
+	};
 
 	return (
-		<KeyboardAvoidingView behavior='padding' style={tw`flex-1 items-center justify-center p-6 bg-white`}>
-			 <StatusBar style='light'/>
-            <Text style={tw`mb-6 text-3xl `}>Create a Signal account</Text>
+		<KeyboardAvoidingView
+			behavior="padding"
+			style={tw`flex-1 items-center justify-center p-6 bg-white`}
+		>
+			<StatusBar style="light" />
+			<Text style={tw`mb-6 text-3xl `}>Create a Signal account</Text>
 
 			<View style={tw`w-[19rem]`}>
 				<Input
 					placeholder="Full Name"
-					autoFocus
 					textContentType="name"
 					value={name}
-                    onChangeText={(text) => setName(text)}
+					onChangeText={(text) => setName(text)}
 				/>
 				<Input
 					placeholder="Email"
 					textContentType="emailAddress"
 					value={email}
-                    onChangeText={(text) => setEmail(text)}
+					onChangeText={(text) => setEmail(text)}
 				/>
 				<Input
 					placeholder="Password"
 					textContentType="password"
-                    secureTextEntry
+					secureTextEntry
 					value={password}
-                    onChangeText={(text) => setPassword(text)}
+					onChangeText={(text) => setPassword(text)}
 				/>
 				<Input
 					placeholder="Profile Picture URL (optional)"
-                    dataDetectorTypes={"link"}
+					dataDetectorTypes={"link"}
 					value={imageUrl}
-                    onChangeText={(text) => setImageUrl(text)}
-                    onSubmitEditing={register}
+					onChangeText={(text) => setImageUrl(text)}
+					onSubmitEditing={register}
 				/>
 			</View>
 
-            <Button raised title={"Register"} color={"#2c6bed"} onPress={register} 
-                style={tw`w-[13rem]`}
-            />
-            <View style={tw`h-[5rem]`}/>
+			<Button
+				raised
+				title={"Register"}
+				color={"#2c6bed"}
+				onPress={register}
+				style={tw`w-[13rem]`}
+			/>   
+			<View style={tw`h-[5rem]`} />
 		</KeyboardAvoidingView>
 	);
 };
