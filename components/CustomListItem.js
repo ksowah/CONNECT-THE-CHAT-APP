8c4d8a789cb6d679940743fc7c5bebbac4b290fd
ksowah <sowahkelvin640@gmail.com>
2022-06-14 16:50:13 +0000

    chat item functionality

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -1,17 +1,39 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Avatar, ListItem } from "@rneui/themed";
 import tw from "twrnc"
+import { collection, onSnapshot, query } from 'firebase/firestore';
+import { db } from '../firebase';
 
 const CustomListItem = ({id, chat, enterChat}) => {
+
+  const [chatMessages, setChatMessages] = useState([])
+  const [latestMessage, setLatestMessage] = useState(0)
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      query(
+        collection(db, "chats", id, "messages")),
+        (snapshot) => {
+          setChatMessages(snapshot.docs)
+          setLatestMessage(chatMessages?.length - 1)
+        }
+      )
+
+    return unsubscribe
+  })
+
   return (
     <ListItem bottomDivider
       onPress={() => enterChat(id, chat.chatName)}
+      key={id}
     >
       <Avatar 
         rounded
         source={{
-          uri: "https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG.png"
+          uri:
+            chatMessages[0]?.data().photoURL ||
+          "https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG.png"
         }}
       />
 
@@ -20,12 +42,7 @@ const CustomListItem = ({id, chat, enterChat}) => {
           {chat?.chatName}
         </ListItem.Title>
         <ListItem.Subtitle numberOfLines={1} ellipsizeMode="tail">
-            This is a test subtitle
-            This is a test subtitle
-            This is a test subtitle
-            This is a test subtitle
-            This is a test subtitle
-            This is a test subtitle
+            {chatMessages[latestMessage]?.data().displayName}: {chatMessages[latestMessage]?.data().message}
         </ListItem.Subtitle>
       </ListItem.Content>
     </ListItem>
