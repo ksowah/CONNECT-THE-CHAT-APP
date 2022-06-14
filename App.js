f2b024c83939cebe3ac2f55b49eecce6336be811
ksowah <sowahkelvin640@gmail.com>
2022-06-14 22:12:41 +0000

    header gradient bg

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -7,13 +6,22 @@ import Register from "./screens/Register";
 import Home from "./screens/Home";
 import AddChat from "./screens/AddChat";
 import ChatScreen from "./screens/ChatScreen";
+import tw from "twrnc"
+import { LinearGradient } from "expo-linear-gradient";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
 
   const globalScreenOptions = {
-    headerStyle: {backgroundColor: "#2c6bed"},
+	headerBackground: () => (
+		<LinearGradient
+		  colors={['#009FFD', '#2A2A72']}
+		  style={{ flex: 1 }}
+		  start={{x: 0, y: 0}}
+		  end={{x: 1, y: 0}}
+		/>
+	  ),
     headerTitleStyle: {color: "white"},
     headerTintColor: "white"
   }
@@ -21,14 +29,12 @@ export default function App() {
 	return (
 		<NavigationContainer>
 			<Stack.Navigator screenOptions={globalScreenOptions}>
-				<Stack.Screen name="Login" component={Login} />
+				<Stack.Screen name="Connect" component={Login} />
 				<Stack.Screen name="Register" component={Register} />
 				<Stack.Screen name="Home" component={Home} />
 				<Stack.Screen name="AddChat" component={AddChat} />
 				<Stack.Screen name="Chat" component={ChatScreen} />
 			</Stack.Navigator>
-		
-			<StatusBar style="auto" />
 		</NavigationContainer>
 	);
 }
